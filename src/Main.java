57de614154852c861df69320dd4b5838595f2ac9
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-08-26 17:27:34 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3};
+
+        for (int i: arr) {
+            System.out.println(i);
+        }
+
+    }
+}
